346d19ce5f9c18c3a327f5374dcbd1847d2377dc
Tapan Karecha <tapan.karecha@bayer.com>
2020-12-25 13:50:38 +0530

    fetch items from webservice in parallel
    - use go routines to fetch item details (reduce load time from 15s to 4s)
    - add comments

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
 
+// endpoint urls
 const (
 	NEW  = "https://hacker-news.firebaseio.com/v0/newstories.json"
 	TOP  = "https://hacker-news.firebaseio.com/v0/topstories.json"
@@ -22,6 +24,7 @@ const (
 )
 
 type Item struct {
+	sno   int    // The serial number of the item as listed on hn
 	Id    int    `json:"id"`    // The item's unique id.
 	Type  string `json:"type"`  // The type of item. One of "job", "story", "comment", "poll", or "pollopt".
 	By    string `json:"by"`    // The username of the item's author.
@@ -35,6 +38,7 @@ var items map[int]Item
 var context = TOP
 var startIndex int
 var countPerPage = 30
+var layout = "01/02 15:04" // reference "2006-01-02 15:04:05.999999999 -0700 MST"
 
 func main() {
 	refresh(true)
@@ -60,9 +64,14 @@ func main() {
 			openItemInBrowser(choice)
 		}
 	}
-
 }
 
+type listItem struct {
+	sno    int
+	itemId int
+}
+
+// refresh loads and lists stories from the context and provides an option to reset startIndex
 func refresh(resetStartIndex bool) {
 	if resetStartIndex {
 		startIndex = 0
@@ -71,6 +80,7 @@ func refresh(resetStartIndex bool) {
 	listStories()
 }
 
+// loadItems uses `context` to load items from the webservice
 func loadItems(endpoint string) {
 	res, err := http.Get(endpoint)
 	if err != nil {
@@ -87,12 +97,21 @@ func loadItems(endpoint string) {
 		log.Fatal(err)
 	}
 	items = map[int]Item{}
+	idChan := make(chan listItem, countPerPage)
+	itemChan := make(chan Item, countPerPage)
+
 	for i, item := range itemIds[startIndex : startIndex+countPerPage] {
-		item := getItem(item)
-		items[i+1+startIndex] = item
+		idChan <- listItem{i + startIndex + 1, item}
+		go getItem(idChan, itemChan)
+	}
+
+	for i := 0; i < countPerPage; i++ {
+		item := <-itemChan
+		items[item.sno] = item
 	}
 }
 
+// listStories formats the prints the contents of items map and should be called after the map is populated by loadItems
 func listStories() {
 	contextName := "unknown context"
 	switch context {
@@ -103,14 +122,16 @@ func listStories() {
 	case BEST:
 		contextName = "best"
 	}
-	fmt.Printf("item %d to %d of %s\n", startIndex+1, startIndex+countPerPage, contextName)
+	fmt.Printf("item %d to %d of %s at %s\n", startIndex+1, startIndex+countPerPage, contextName, time.Now().Format(layout))
 	for i := startIndex + 1; i <= startIndex+countPerPage; i++ {
-		fmt.Printf("%02d. [%5d %15s] %s\n", i, items[i].Score, items[i].By, items[i].Title)
+		fmt.Printf("%02d. [%s %4d %15s] %s\n", i, time.Unix(int64(items[i].Time), 0).Format(layout), items[i].Score, items[i].By, items[i].Title)
 	}
 }
 
-func getItem(id int) Item {
-	res, err := http.Get(fmt.Sprintf(ITEM, id))
+// getItem gets the details of an item based on item id and is expected to be run in a go routine
+func getItem(listItemChan chan listItem, itemChan chan Item) {
+	li := <-listItemChan
+	res, err := http.Get(fmt.Sprintf(ITEM, li.itemId))
 	if err != nil {
 		panic(err.Error())
 	}
@@ -124,9 +145,11 @@ func getItem(id int) Item {
 	if err != nil {
 		panic(err.Error())
 	}
-	return item
+	item.sno = li.sno
+	itemChan <- item
 }
 
+// getInput presents available options to user and returns the option provided by user
 func getInput() string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("enter choice [<sno>|(m)ore|(t)op|(b)est|(n)ew|(q)uit|(r)efresh]: ")
@@ -134,6 +157,8 @@ func getInput() string {
 	return strings.Trim(text, "\n \t")
 }
 
+// openItemInBrowser opens the hn page for a given item, and it assumes the application is running on MacOS and also
+// Firefox is installed. Change the command to suite your environment.
 func openItemInBrowser(ch string) {
 	if choice, err := strconv.Atoi(ch); err == nil {
 		if item, ok := items[choice]; ok {
